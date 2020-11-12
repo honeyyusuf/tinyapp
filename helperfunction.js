96823b35222be099342265fe536da7ea8a0a9c3b
honeyyusuf <yusufhon0@gmail.com>
2020-11-12 19:50:34 +0000

    final vesion of helperfunction

--- helperfunction.js
+++ helperfunction.js
@@ -3,12 +3,13 @@
 
 const bcrypt = require('bcryptjs');
 
-
+////// used to generate shortid for new urls and new userid for regstration///////
 function generateRandomString() {
   let shorran = Math.random().toString(36).substring(2, 8);
   return shorran;
-}
- 
+};
+
+/////////// check if the email and password macths user database user tries to login///
 const usercheck = (objs, email, password) => {
   if (email === '' || password === '') {
     return {error : "Please input the email and password"};
@@ -26,6 +27,7 @@ const usercheck = (objs, email, password) => {
     return {error:'email', obj:null};
   }
 };
+/////////checkes if the email exists for in the userdatabase during regstration/////////
 const getUserByEmail = (email,users) => {
   for (let user in users) {
     if (users[user].email === email) {
@@ -35,7 +37,7 @@ const getUserByEmail = (email,users) => {
   return null;
 };
 
-
+///////////checkes if the id is the same as the urls id ///////
 const urlsForUser = (objs,id) => {
   let userURL = {};
   for (let obj in objs) {
